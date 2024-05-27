901bd0b8f2885090c98d1045fa5ee066ece01ee3
Биянхо Дамир Джумарович <@step.kz>
2024-05-27 19:06:39 +0500

    done with image

--- src/pages/SearchBook.jsx
+++ src/pages/SearchBook.jsx
@@ -1,12 +1,47 @@
-import { Input, Space, Button, ConfigProvider } from 'antd';
-const { Search } = Input;
+import React, { useState } from 'react';
+import { Input, Card, Row, Col } from 'antd';
+import axios from 'axios';
 
-export const SearchBook=()=>{
-    <Search
-        placeholder="введите название"
+const { Search } = Input;
+const { Meta } = Card;
+
+export const SearchBook = () => {
+  const [results, setResults] = useState([]);
+
+  const onSearch = async (value) => {
+    try {
+      const response = await axios.get(`https://bookmate.ru/node-api/p-graphql/?query=${value}`);
+      console.log("SUCCESS", response.data);
+      setResults(response.data.data.search.books);
+    } catch (error) {
+      console.error('Ошибка при выполнении запроса:', error);
+    }
+  };
+
+  return (
+    <>
+      <Search
+        placeholder="Введите название книги"
         allowClear
         enterButton="Поиск"
-        style={{ width: 300 }}
         onSearch={onSearch}
       />
-}
+      <Row gutter={[16, 16]}>
+        {results.map((book) => (
+          <Col key={book.id} xs={24} sm={12} md={8} lg={6} xl={4}>
+            <Card
+              hoverable
+              style={{ width: 200 }}
+            >
+              <Meta title={book.title} />
+              <p>Автор: {book.authors.map(author => author.name).join(', ')}</p>
+              <p>Дата выхода: {book.publishedAt}</p>
+              <p>Популярность: {book.popularity}</p>
+              {/* Добавьте другие параметры по вашему выбору */}
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
--- src/pages/SearchImage.jsx
+++ src/pages/SearchImage.jsx
@@ -1,12 +1,49 @@
-import { Input, Space, Button, ConfigProvider } from 'antd';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Input, Card, Row, Col } from 'antd';
+
 const { Search } = Input;
 
-export const SearchImage=()=>{
-    <Search
-        placeholder="введите название"
-        allowClear
+export const SearchImage = () => {
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState([]);
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`https://api.unsplash.com/search/photos?query=${query}&client_id=YOUR_ACCESS_KEY`);
+      setImages(response.data.results);
+    } catch (error) {
+      console.error('Ошибка при выполнении запроса:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <div>
+      <Search
+        placeholder="Введите запрос"
         enterButton="Поиск"
-        style={{ width: 300 }}
-        onSearch={onSearch}
+        size="large"
+        value={query}
+        onChange={handleInputChange}
+        onSearch={handleSearch}
       />
-}
+      <Row gutter={[16, 16]}>
+        {images.map((image) => (
+          <Col key={image.id} xs={24} sm={12} md={8} lg={6}>
+            <Card
+              hoverable
+              style={{ marginBottom: 16 }}
+              cover={<img alt={image.alt_description} src={image.urls.small} style={{ height: '200px', objectFit: 'cover' }} />}
+            >
+              <Card.Meta title={image.alt_description} />
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
